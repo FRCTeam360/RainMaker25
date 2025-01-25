58201421524097146ba85f3f5b7ff1cfc8fb3b75
annashimshock <122415457+annashimshock@users.noreply.github.com>
2025-01-24 18:23:17 -0800

    bottom switch getter

--- src/main/java/frc/robot/subsystems/Elevator/ElevatorIOWB.java
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorIOWB.java
@@ -91,8 +91,4 @@ public class ElevatorIOWB implements ElevatorIO {
     public void setElevatorPostion(double height) {
         elevatorMotor.setPosition(height);
     }
-
-    public boolean getBottomSwitch() {
-        return !bottomSwitch.get();
-    }
 }

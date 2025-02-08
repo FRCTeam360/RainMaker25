610ba3671320764eb9389494ef9fb967b3bdae18
RaineyRubin <145077026+RaineyRubin@users.noreply.github.com>
2025-02-08 14:09:41 -0800

    added command name to logCommand

--- src/main/java/frc/CommandLogger.java
+++ src/main/java/frc/CommandLogger.java
@@ -9,12 +9,12 @@ import edu.wpi.first.wpilibj2.command.Command;
  * Public: Logging when a command given to the robot begins and when it ends or is interupted
  */
 public class CommandLogger {
-  public static Command logCommand(Command command) {
-    return command.beforeStarting(() -> logCommand(command, true))
-        .finallyDo(() -> logCommand(command, false));
+  public static Command logCommand(Command command, String commandName) {
+    return command.beforeStarting(() -> logCommand(commandName, true))
+        .finallyDo(() -> logCommand(commandName, false));
   }
 
-  private static void logCommand(Command command, boolean isRunning) {
-    Logger.recordOutput("CommandRunning " + command.getName(), isRunning);
+  private static void logCommand(String commandName, boolean isRunning) {
+    Logger.recordOutput("CommandRunning " + commandName, isRunning);
   }
 }

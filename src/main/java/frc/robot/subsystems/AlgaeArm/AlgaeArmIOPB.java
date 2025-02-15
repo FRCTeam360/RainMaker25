8290a76e17ff0834c61fb4222f7d43517862fab6
RaineyRubin <145077026+RaineyRubin@users.noreply.github.com>
2025-02-15 14:03:55 -0800

    algea arm pid controller replaced

--- src/main/java/frc/robot/subsystems/AlgaeArm/AlgaeArmIOPB.java
+++ src/main/java/frc/robot/subsystems/AlgaeArm/AlgaeArmIOPB.java
@@ -12,10 +12,13 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.EncoderConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
@@ -27,14 +30,24 @@ public class AlgaeArmIOPB implements AlgaeArmIO {
 
   private final SparkMax armMotor = new SparkMax(Constants.PracticeBotConstants.ALGAE_ARM_ID, MotorType.kBrushless); // placeholder                                                                                                                    // ID
   private final RelativeEncoder encoder = armMotor.getEncoder();
-  private final PIDController pid = new PIDController(0, 0, 0); // TODO: find pid values
+  
+  private final double kP = 0.0;
+  private final double kI = 0.0;
+  private final double kD = 0.0;
 
+  private final double positionConversionFactor = 1.0;
   private final SparkMaxConfig sparkMaxConfig = new SparkMaxConfig();
 
   /** Creates a new AlgaeArmIOPB. */
   public AlgaeArmIOPB() {
     sparkMaxConfig.idleMode(IdleMode.kBrake);
     sparkMaxConfig.inverted(false);
+    ClosedLoopConfig closedLoopConfig = new ClosedLoopConfig();
+    closedLoopConfig.pid(kP, kI, kD);
+    sparkMaxConfig.apply(closedLoopConfig);
+    EncoderConfig encoderConfig = new EncoderConfig();
+    encoderConfig.positionConversionFactor(positionConversionFactor);
+    sparkMaxConfig.apply(encoderConfig);
     armMotor.configure(sparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
@@ -50,6 +63,6 @@ public class AlgaeArmIOPB implements AlgaeArmIO {
   }
 
   public void setPosition(double position) {
-    pid.setSetpoint(position);
+    armMotor.getClosedLoopController().setReference(position, ControlType.kPosition);
   }
 }

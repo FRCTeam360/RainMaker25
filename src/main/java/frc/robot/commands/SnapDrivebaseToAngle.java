384af0fe491a1262680d3186ac652820e4e4d1a6
RaineyRubin <145077026+RaineyRubin@users.noreply.github.com>
2025-02-16 16:38:53 -0800

    fixing snapDriveBasToAngle

--- src/main/java/frc/robot/commands/SnapDrivebaseToAngle.java
+++ src/main/java/frc/robot/commands/SnapDrivebaseToAngle.java
@@ -6,6 +6,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -27,10 +28,12 @@ public class SnapDrivebaseToAngle extends Command {
     private CommandSwerveDrivetrain driveTrain;
     private CommandXboxController driverCont = new CommandXboxController(0);
     private double angleToFace = 0.0;
-    private Vision vision;
     private Rotation2d angleToFaceRotation2d;
     private Vision aprilTagID;
     private boolean endEarly;
+    private int pipeline;
+
+    private final String CMD_NAME = "SnapDrivebaseToAngle: ";
 
     private static final Map<Integer, Double> tagIDToAngle = Map.ofEntries(
             Map.entry(21, 180.0),
@@ -47,8 +50,10 @@ public class SnapDrivebaseToAngle extends Command {
             Map.entry(8, -120.0));
 
     /** Creates a new SnapDrivebaseToAngle. */
-    public SnapDrivebaseToAngle(CommandSwerveDrivetrain driveTrain) {
+    public SnapDrivebaseToAngle(Vision aprilTagID, CommandSwerveDrivetrain driveTrain, int pipeline) {
+        this.aprilTagID = aprilTagID;
         this.driveTrain = driveTrain;
+        this.pipeline = pipeline;
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(driveTrain);
     }
@@ -56,52 +61,69 @@ public class SnapDrivebaseToAngle extends Command {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+
+        aprilTagID.setPipeline(pipeline);
+
         double angleToFace = driveTrain.getAngle();
-        int priorityID = vision.getAprilTagID();
+        int priorityID = aprilTagID.getAprilTagID();
+
         endEarly = true;
-        if (vision.getTV() == 1 && tagIDToAngle.containsKey(priorityID)) {
+        if (aprilTagID.getTV() == 1 && tagIDToAngle.containsKey(priorityID)) {
             endEarly = false;
             angleToFace = tagIDToAngle.get(priorityID);
-
-            angleToFaceRotation2d = Rotation2d.fromDegrees(angleToFace);
-
-            LimelightHelpers.setPriorityTagID("limelight", priorityID);
         }
-        // if (driveTrain.getAngle() >= -30.0 || driveTrain.getAngle() <= 30.0) {
-        // angleToFace = 0.0;
-        // } else if (driveTrain.getAngle() <= -30.0 || driveTrain.getAngle() >= -90.0)
-        // {
-        // angleToFace = -60.0;
-        // } else if (driveTrain.getAngle() <= -90.0 || driveTrain.getAngle() >= -150.0)
-        // {
-        // angleToFace = -120.0;
-        // } else if (driveTrain.getAngle() <= -150.0 || driveTrain.getAngle() >= 150.0)
-        // {
-        // angleToFace = 180.0;
-        // } else if (driveTrain.getAngle() <= 150.0 || driveTrain.getAngle() >= 90.0) {
-        // angleToFace = 120.0;
-        // } else if (driveTrain.getAngle() >= 30.0 || driveTrain.getAngle() <= 90.0) {
-        // angleToFace = 60.0;
-        // }
+
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+        if (alliance.isPresent() && alliance.get() == Alliance.Red) {
+            if (angleToFace <= 0.0) {
+                angleToFace = angleToFace + 180.0;
+            } else {
+                angleToFace = angleToFace - 180.0;
+            }
+        }
+
+        angleToFaceRotation2d = Rotation2d.fromDegrees(angleToFace);
+
+        Logger.recordOutput(CMD_NAME + "AngleToFace", angleToFace);
+
+        LimelightHelpers.setPriorityTagID("limelight", priorityID);
+
+    }
+
+    public static Translation2d rotateTranslation(
+        Translation2d translationToRotate,
+        Rotation2d rotation
+    ) {
+        return translationToRotate.rotateBy(rotation);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        driveTrain.driveFieldCentricFacingAngle(
+        if (endEarly) return;
+
+        if (aprilTagID.getTV() == 1) {
+            driveTrain.driveFieldCentricFacingAngle(
                 Math.pow(MathUtil.applyDeadband(-driverCont.getLeftY(), 0.1), 2.0),
                 Math.pow(MathUtil.applyDeadband(-driverCont.getLeftX(), 0.1), 2.0),
                 angleToFace);
+        } else {
+            driveTrain.driveFieldCentricFacingAngle(0.0, 0.0, angleToFaceRotation2d.getDegrees());
+        }
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        LimelightHelpers.setPriorityTagID("limelight", -1);
+        driveTrain.xOut();
     };
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return driveTrain.isAtRotationSetpoint();
+        boolean onHeading = driveTrain.isAtRotationSetpoint();
+        Logger.recordOutput(CMD_NAME + "onHeading", onHeading);
+        return endEarly || driveTrain.isAtRotationSetpoint();
     }
 }

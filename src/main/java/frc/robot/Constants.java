9d1fd28f1a6175ac403c36f139baa45ec3a03f94
annashimshock <122415457+annashimshock@users.noreply.github.com>
2025-02-25 17:00:18 -0800

    added comment

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -146,7 +146,7 @@ public final class Constants {
 
     }
 
-    public static final class CompBotConstants {
+    public static final class CompBotConstants { // Currently just a copy of practice bot, values should be adjusted to comp if needed
         public static final String CANBUS_NAME = "Default Name";
         public static final int BACK_ELEVATOR_ID = 14;
         public static final int FRONT_ELEVATOR_ID = 15;

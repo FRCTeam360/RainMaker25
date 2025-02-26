6dd216bfdcb2c6c16c26feb6890660d2c206ba38
James Taylor <james.taylor2@comcast.net>
2025-02-25 19:15:07 -0800

    FIRST 3/3 RUN EVER !!

--- src/main/java/frc/robot/subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision/Vision.java
@@ -54,8 +54,8 @@ public class Vision extends SubsystemBase {
   private static final InterpolatingMatrixTreeMap<Double, N3, N1> MEASUREMENT_STD_DEV_DISTANCE_MAP = new InterpolatingMatrixTreeMap<>();
 
   static {
-    MEASUREMENT_STD_DEV_DISTANCE_MAP.put(1.0, VecBuilder.fill(1.5, 1.5, 50.0));
-    MEASUREMENT_STD_DEV_DISTANCE_MAP.put(8.0, VecBuilder.fill(10.0, 10.0, 100.0));
+    MEASUREMENT_STD_DEV_DISTANCE_MAP.put(1.0, VecBuilder.fill(1.5, 1.5, 999999.0));
+    MEASUREMENT_STD_DEV_DISTANCE_MAP.put(8.0, VecBuilder.fill(10.0, 10.0, 999999.0));
   }
 
   private static final Matrix<N3, N1> stdDevMatrix = VecBuilder.fill(3.0, 3.0, 999999.0);

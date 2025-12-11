367a00830119e4557578dae3307df2111852085b
Kaleb-Robotics <145076363+Kaleb-Robotics@users.noreply.github.com>
2025-12-10 18:52:34 -0800

    quest nav subsystem and instantiated

---
+++ src/main/java/frc/robot/subsystems/QuestNav/QuestNavSubsystem.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.QuestNav;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import gg.questnav.questnav.QuestNav;
+
+public class QuestNavSubsystem extends SubsystemBase {
+    QuestNav questNav = new QuestNav();
+  /** Creates a new QuestNav. */
+  public QuestNavSubsystem() {}
+
+  @Override
+  public void periodic() {
+    questNav.commandPeriodic();
+  }
+}
